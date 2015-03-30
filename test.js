9c7e0176ad2673333beee9c303d7b506e00324ae
woodenToaster <litmusik@yahoo.com>
2015-03-30 14:48:03 -0500

    Add tests and make functions from timeouts and conditionals

---
+++ test.js
@@ -0,0 +1,9 @@
+setInterval(function() {
+  heal();
+  }, 2000
+);
+if(enemy < 2) {
+  attack();
+}
+
+//# sourceMappingURL=test.js.map
---
+++ test/timeouts.js
@@ -0,0 +1,16 @@
+var should = require('should');
+var compileLine = require('./helpers.js').compileLine;
+
+describe('Timeouts', function() {
+  describe('basic timeout', function() {
+    it('should recognize timeout with function as ID', function() {
+      var result = compileLine("every 2000: attack");
+      should.strictEqual(result, "(function(){setInterval(function(){this.attack();},2000);)\n");
+    });
+
+    it('should recognize timeout with function as propertychain', function() {
+      var result = compileLine("every 2000: player.attack");
+      should.strictEqual(result, "(function(){setInterval(function(){this.player.attack();},2000);)\n");
+    });
+  });
+});
