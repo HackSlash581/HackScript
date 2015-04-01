9ab66f6f1c71b65a9c8b30671ea545283c7b6096
woodenToaster <litmusik@yahoo.com>
2015-04-01 08:56:44 -0500

    All compilation produces functions. All tests passing

--- test/conditionals.js
+++ test/conditionals.js
@@ -7,11 +7,16 @@ describe('Conditionals', function() {
     it('should recognize if INT < INT: attack', function() {
       var result = compileLine("if 5 < 6: attack");
       should.strictEqual(result, "(function(){if(5<6){this.attack();})\n");
-    })
+    });
 
-    it('should recognzize if ID < INT: attack', function() {
-      var result = compileLine("if range < 10: attack");
-      should.strictEqual(result, "if(range < 10) {\n  attack();\n}\n");
-    })
-  })
-})
+    it('should recognzize if ID < INT: heal', function() {
+      var result = compileLine("if health < 10: heal");
+      should.strictEqual(result, "(function(){if(this.health<10){this.heal();})\n");
+    });
+
+    it('should recognize if ID < ID: heal', function() {
+      var result = compileLine("if health < ammo: heal");
+      should.strictEqual(result, "(function(){if(this.health<this.ammo){this.heal();})\n");
+    });
+  });
+});
