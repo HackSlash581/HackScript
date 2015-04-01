f0f7673f5e4740891f79c6b7afd0da29cdb729e5
woodenToaster <litmusik@yahoo.com>
2015-04-01 09:25:54 -0500

    Fix missing brace

--- test/conditionals.js
+++ test/conditionals.js
@@ -6,17 +6,17 @@ describe('Conditionals', function() {
   describe('with simple expressions', function() {
     it('should recognize if INT < INT: attack', function() {
       var result = compileLine("if 5 < 6: attack");
-      should.strictEqual(result, "(function(){if(5<6){this.attack();})\n");
+      should.strictEqual(result, "(function(){if(5<6){this.attack();}})\n");
     });
 
     it('should recognzize if ID < INT: heal', function() {
       var result = compileLine("if health < 10: heal");
-      should.strictEqual(result, "(function(){if(this.health<10){this.heal();})\n");
+      should.strictEqual(result, "(function(){if(this.health<10){this.heal();}})\n");
     });
 
     it('should recognize if ID < ID: heal', function() {
       var result = compileLine("if health < ammo: heal");
-      should.strictEqual(result, "(function(){if(this.health<this.ammo){this.heal();})\n");
+      should.strictEqual(result, "(function(){if(this.health<this.ammo){this.heal();}})\n");
     });
   });
 });
