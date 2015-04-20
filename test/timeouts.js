7086ef4d8d1b3d1c5bc7f07d0f6d94750fff6435
woodenToaster <litmusik@yahoo.com>
2015-04-20 13:46:35 -0500

    Fix missing '}' in timeout generation

--- test/timeouts.js
+++ test/timeouts.js
@@ -5,12 +5,12 @@ describe('Timeouts', function() {
   describe('basic timeout', function() {
     it('should recognize timeout with function as ID', function() {
       var result = compileLine("every 2000: attack");
-      should.strictEqual(result, "(function(){setInterval(function(){this.attack();},2000);)\n");
+      should.strictEqual(result, "(function(){setInterval(function(){this.attack();},2000);})\n");
     });
 
     it('should recognize timeout with function as propertychain', function() {
       var result = compileLine("every 2000: player.attack");
-      should.strictEqual(result, "(function(){setInterval(function(){this.player.attack();},2000);)\n");
+      should.strictEqual(result, "(function(){setInterval(function(){this.player.attack();},2000);})\n");
     });
   });
 });
